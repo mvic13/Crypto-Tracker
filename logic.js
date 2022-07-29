695fd6a30993f41242fea4cd5cf54aa64fd57dfc
Nllano777 <nllbr99@gmail.com>
2022-07-28 21:28:28 -0500

    fixed commit

--- logic.js
+++ logic.js
@@ -1,62 +1,106 @@
 var options = {
-    chart: {
-      type: 'bar',
-      width: "250px",
-      height: "250px",
-      border: "25px solid black",
+
+  chart:{
+    height:"500px",
+    width:"500px",
+    type:"candlestick",
+  },
+  series: [{
+    data: [{
+      x: new Date(2020, 01, 01),
+      y: [51.98, 56.29, 51.59, 53.85]
     },
-    series: [{
-      name: 'sales',
-      data: [30,40,35,50,49,60,70,91,125]
-    }],
-    xaxis: {
-      categories: [1991,1992,1993,1994,1995,1996,1997, 1998,1999]
-    }
-  }
-  
-  
-  var chart = new ApexCharts(document.querySelector("#chart1"), options);
-  
-  chart.render();
-  var options = {
-    chart: {
-      type: 'bar',
-      width: "250px",
-      height: "250px",
-      border: "25px solid black",
+    {
+      x: new Date(2020, 02, 01),
+      y: [53.66, 54.99, 51.35, 52.95]
     },
-    series: [{
-      name: 'sales',
-      data: [30,40,35,50,49,60,70,91,125]
-    }],
-    xaxis: {
-      categories: [1991,1992,1993,1994,1995,1996,1997, 1998,1999]
-    }
-  }
-  
-  
-  var chart = new ApexCharts(document.querySelector("#chart2"), options);
-  
-  chart.render();
-  var options = {
-    chart: {
-      type: 'bar',
-      width: "250px",
-      height: "250px",
-      border: "25px solid black",
+    {
+      x: new Date(2020, 08, 01),
+      y: [52.76, 57.35, 52.15, 57.03]
+    }]
+  }],
+ };
+
+  var chart = new ApexCharts(document.querySelector("#candlestick1"), options);
+chart.render();  
+
+var options = {
+
+  chart:{
+    height:"500px",
+    width:"500px",
+    type:"candlestick",
+  },
+  series: [{
+    data: [{
+      x: new Date(2020, 01, 01),
+      y: [51.98, 56.29, 51.59, 53.85]
     },
-    series: [{
-      name: 'sales',
-      data: [30,40,35,50,49,60,70,91,125]
-    }],
-    xaxis: {
-      categories: [1991,1992,1993,1994,1995,1996,1997, 1998,1999]
-    }
-  }
-  
-  
-  var chart = new ApexCharts(document.querySelector("#chart3"), options);
-  
-  chart.render();
+    {
+      x: new Date(2020, 02, 01),
+      y: [53.66, 54.99, 51.35, 52.95]
+    },
+    {
+      x: new Date(2020, 08, 01),
+      y: [52.76, 57.35, 52.15, 57.03]
+    }]
+  }],
+ };
+
+  var chart = new ApexCharts(document.querySelector("#candlestick2"), options);
+chart.render();  
+
+var options = {
+
+  chart:{
+    height:"500px",
+    width:"500px",
+    type:"candlestick",
+  },
+  series: [{
+    data: [{
+      x: new Date(2020, 01, 01),
+      y: [51.98, 56.29, 51.59, 53.85]
+    },
+    {x: new Date(2020, 02, 01),
+      y: [53.66, 54.99, 51.35, 52.95]
+    },
+    {
+      x: new Date(2020, 08, 01),
+      y: [52.76, 57.35, 52.15, 57.03]
+    }]
+  }],
+ };
+
+  var chart = new ApexCharts(document.querySelector("#candlestick3"), options);
+chart.render();  
+
+var options = {
+
+  chart:{
+    height:"500px",
+    width:"500px",
+    type:"candlestick",
+  },
+  series: [{
+    data: [{
+      x: new Date(2020, 01, 01),
+      y: [51.98, 56.29, 51.59, 53.85]
+    },
+    {
+      x: new Date(2020, 02, 01),
+      y: [53.66, 54.99, 51.35, 52.95]
+    },
+    {
+      x: new Date(2020, 08, 01),
+      y: [52.76, 57.35, 52.15, 57.03]
+    }]
+  }],
+ };
+
+  var chart = new ApexCharts(document.querySelector("#candlestick4"), options);
+chart.render();  
 
 
+
+  
