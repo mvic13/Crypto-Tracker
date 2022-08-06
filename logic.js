39001e204b8c0675a6d68534fa18ad3a2d7a7ce3
Nllano777 <nllbr99@gmail.com>
2022-08-06 17:02:32 -0500

    charts-update with search-bar

--- logic.js
+++ logic.js
@@ -1,18 +1,19 @@
 const APIKey = "6LT2XMVTOVBKRMI4"
 const interval = "5min";
 var APIURL = "https://www.alphavantage.co/query?apikey=6LT2XMVTOVBKRMI4&function=TIME_SERIES_MONTHLY_ADJUSTED&datatype=json";
+var APIURL2 = "https://www.alphavantage.co/query?function=NEWS_SENTIMENT&tickers=COIN,CRYPTO:BTC,FOREX:USD&time_from=20220410T0130&limit=200&6LT2XMVTOVBKRMI4=json";
 
 var timeSeries = [];
 
-var button = document.getElementById("get_data")
-button.addEventListener('click', (event) => {
-  event.preventDefault()
-  var input = document.getElementById("companies");
-  var symbol = input.value;
-  var indtraday = document.getElementById("timeinterval")
-  indtraday.innerHTML = `Symbol: ${symbol}`;
-  GetData(symbol);
-})
+// var button = document.getElementById("get_data")
+// button.addEventListener('click', (event) => {
+//   event.preventDefault()
+//   var input = document.getElementById("companies");
+//   var symbol = input.value;
+//   var indtraday = document.getElementById("timeinterval")
+//   indtraday.innerHTML = `Symbol: ${symbol}`;
+//   GetData(symbol);
+// })
 
 function GetData(symbol, chart) {
   var url = `${APIURL}&symbol=${symbol}&interval=${interval}&apikey=${APIKey}`;
@@ -34,7 +35,17 @@ function GetData(symbol, chart) {
       chart.updateSeries([{ data: timeSeries.reverse() }], true);
       console.log(timeSeries);
     })
-}
+};
+
+function Tickers() {
+  var url = `${APIURL2}&symbol=${symbol}&interval=${interval}&apikey=${APIKey}`;
+  fetch(url)
+    .then(data => data.json())
+    .then(data => {
+      var series = data["Monthly Adjusted Time Series"];
+      var keys = Object.keys(series);
+    })
+};
 
 var options = {
 
@@ -64,5 +75,22 @@ GetData("AAPL", chart_1);
 GetData("MSFT", chart_2);
 GetData("IBM", chart_3);
 GetData("TSLA", chart_4);
+
+
+// const searchBar = document.getElementById('searchBar');
+// let UserInput = [];
+
+// searchBar.addEventListener('keyup', (e) => {
+//   const searchString = e.target.value.toUpperCase();
+
+//   const UserInput = searchBar.filter((symbol) => {
+//     return (
+//       symbol.length.toUpperCase().includes(searchString),
+//       UserInput.value.toUpperCase().includes(searchString)
+//     );
+//   });
+//   APIURL2 = APIURL.charAt.toString(localStorage(Symbol))
+// });
+
 
 
